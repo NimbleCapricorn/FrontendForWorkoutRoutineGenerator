1741d0b1b12a512e04fa2aa9e8e661caee839d18
Nimble Capricorn <Nimble.capricorn@gmail.com>
2023-10-25 17:34:14 +0200

    initialized the repo

---
+++ src/chatGPTTestVersion.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import axios from axios
+
+const App = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch('http://localhost:8000/items');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setItems(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchItems();
+  }, []);
+
+  return (
+    <div>
+      <h1>Items</h1>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
